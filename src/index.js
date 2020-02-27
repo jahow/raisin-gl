cd344914a997e884a0c8302322aafbe3fcbc81f0
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-02-27 16:34:38 +0100

    very basic rendering capabilities

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
  *
  * @param {HTMLElement} htmlElement Container element for the scene; sizing will be kept
  */
+import {createProgramFromSources, resizeCanvasToDisplaySize} from './utils'
+
 export function createScene(htmlElement) {
   const canvas = document.createElement('canvas');
   canvas.style.width = '100%';
@@ -9,4 +11,36 @@ export function createScene(htmlElement) {
   htmlElement.appendChild(canvas);
 
   const gl = canvas.getContext('webgl2');
+
+  const vs = `#version 300 es
+void main() {
+  gl_Position = vec4(gl_VertexID >> 1, gl_VertexID & 1, 0.0, 1.0) * 2.0 - 1.0;
+}
+`;
+
+  const fs = `#version 300 es
+precision mediump float;
+
+uniform vec2 u_resolution;
+
+out vec4 outColor;
+
+void main() {
+  outColor = vec4(1, gl_FragCoord.x / u_resolution.x, gl_FragCoord.y / u_resolution.y, 1);
+}
+`;
+
+  // setup GLSL program
+  const program = createProgramFromSources(gl, [vs, fs]);
+  const resolutionLoc = gl.getUniformLocation(program, 'u_resolution');
+
+  // draw
+  resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  gl.useProgram(program);
+
+  gl.uniform2f(resolutionLoc, gl.canvas.width, gl.canvas.height);
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
---
+++ src/utils.js
@@ -0,0 +1,127 @@
+const defaultShaderType = [
+  "VERTEX_SHADER",
+  "FRAGMENT_SHADER",
+];
+
+/**
+ * Wrapped logging function.
+ * @param {string} msg The message to log.
+ */
+function error(msg) {
+  console.error(msg);
+}
+
+/**
+ * Loads a shader.
+ * @param {WebGLRenderingContext} gl The WebGLRenderingContext to use.
+ * @param {string} shaderSource The shader source.
+ * @param {number} shaderType The type of shader.
+ * @param {function} [opt_errorCallback] callback for errors.
+ * @return {WebGLShader} The created shader.
+ */
+function loadShader(gl, shaderSource, shaderType, opt_errorCallback) {
+  const errFn = opt_errorCallback || error;
+  // Create the shader object
+  const shader = gl.createShader(shaderType);
+
+  // Load the shader source
+  gl.shaderSource(shader, shaderSource);
+
+  // Compile the shader
+  gl.compileShader(shader);
+
+  // Check the compile status
+  const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!compiled) {
+    // Something went wrong during compilation; get the error
+    const lastError = gl.getShaderInfoLog(shader);
+    errFn("*** Error compiling shader '" + shader + "':" + lastError);
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+/**
+ * Creates a program, attaches shaders, binds attrib locations, links the
+ * program and calls useProgram.
+ * @param {WebGLRenderingContext} gl The WebGLRenderingContext
+ *        to use.
+ * @param {WebGLShader[]} shaders The shaders to attach
+ * @param {string[]} [opt_attribs] An array of attribs names. Locations will be assigned by index if not passed in
+ * @param {number[]} [opt_locations] The locations for the. A parallel array to opt_attribs letting you assign locations.
+ * @param {function} [opt_errorCallback] callback for errors. By default it just prints an error to the console
+ *        on error. If you want something else pass an callback. It's passed an error message.
+ */
+function createProgram(
+  gl, shaders, opt_attribs, opt_locations, opt_errorCallback) {
+  const errFn = opt_errorCallback || error;
+  const program = gl.createProgram();
+  shaders.forEach(function(shader) {
+    gl.attachShader(program, shader);
+  });
+  if (opt_attribs) {
+    opt_attribs.forEach(function(attrib, ndx) {
+      gl.bindAttribLocation(
+        program,
+        opt_locations ? opt_locations[ndx] : ndx,
+        attrib);
+    });
+  }
+  gl.linkProgram(program);
+
+  // Check the link status
+  const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!linked) {
+    // something went wrong with the link
+    const lastError = gl.getProgramInfoLog(program);
+    errFn("Error in program linking:" + lastError);
+
+    gl.deleteProgram(program);
+    return null;
+  }
+  return program;
+}
+
+/**
+ * Creates a program from 2 sources.
+ * @param {WebGLRenderingContext} gl The WebGLRenderingContext
+ *        to use.
+ * @param {string[]} shaderSources Array of sources for the
+ *        shaders. The first is assumed to be the vertex shader,
+ *        the second the fragment shader.
+ * @param {string[]} [opt_attribs] An array of attribs names. Locations will be assigned by index if not passed in
+ * @param {number[]} [opt_locations] The locations for the. A parallel array to opt_attribs letting you assign locations.
+ * @param {function} [opt_errorCallback] callback for errors. By default it just prints an error to the console
+ *        on error. If you want something else pass an callback. It's passed an error message.
+ * @return {WebGLProgram} The created program.
+ */
+export function createProgramFromSources(
+  gl, shaderSources, opt_attribs, opt_locations, opt_errorCallback) {
+  const shaders = [];
+  for (let ii = 0; ii < shaderSources.length; ++ii) {
+    shaders.push(loadShader(
+      gl, shaderSources[ii], gl[defaultShaderType[ii]], opt_errorCallback));
+  }
+  return createProgram(gl, shaders, opt_attribs, opt_locations, opt_errorCallback);
+}
+
+/**
+ * Resize a canvas to match the size its displayed.
+ * @param {HTMLCanvasElement} canvas The canvas to resize.
+ * @param {number} [multiplier] amount to multiply by.
+ *    Pass in window.devicePixelRatio for native pixels.
+ * @return {boolean} true if the canvas was resized.
+ */
+export function resizeCanvasToDisplaySize(canvas, multiplier) {
+  multiplier = multiplier || 1;
+  const width  = canvas.clientWidth  * multiplier | 0;
+  const height = canvas.clientHeight * multiplier | 0;
+  if (canvas.width !== width ||  canvas.height !== height) {
+    canvas.width  = width;
+    canvas.height = height;
+    return true;
+  }
+  return false;
+}
