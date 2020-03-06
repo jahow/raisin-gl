cfc3a7346285ecdfc7b123605cfad26d4f37133e
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-03-06 17:55:10 +0100

    reworked color palette and layout

--- examples/index.js
+++ examples/index.js
@@ -12,20 +12,24 @@ import {
 const renderer = createRenderer(document.getElementById('example-1'))
 
 let scene = createScene()
-scene = addPrimitive(scene, makeUnion(
-  makeCircle(-30, -15, 50),
-  makeCircle(30, -22, 30)
+scene = addPrimitive(scene,
+  applyPaint(
+    makeUnion(
+      makeCircle(-30, -15, 50),
+      makeCircle(30, -22, 30)
+    ),
+    makeSolidPaint(0.9059, 0.4431, 0.4902, 0.8)
 ))
 scene = addPrimitive(scene,
   applyPaint(
     makeBox(25, 15, 100, 100),
-    makeSolidPaint(0.45, 0.88, 1, 0.7)
+    makeSolidPaint(0.6863, 0.8235, 0.4589, 0.8)
   )
 )
 scene = addPrimitive(scene,
   applyPaint(
     makeBox(68, 15, 120, 70),
-    makeSolidPaint(1, 0.2, 0.1, 0.7)
+    makeSolidPaint(39/255, 198/255, 210/255, 0.8)
   )
 )
 
